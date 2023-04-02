c0c765675333cfae15f146685db3644ed514064e
Sergei Matveev <msergne@gmail.com>
2023-04-02 21:33:00 +0300

    Add Зад-4

---
+++ Lesson-7/Зад-4/Matrix.py
@@ -0,0 +1,42 @@
+"""
+Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса
+(метод init()), который должен принимать данные (список списков) для формирования матрицы.
+Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+Следующий шаг — реализовать перегрузку метода str() для вывода матрицы в привычном виде.
+Далее реализовать перегрузку метода add() для реализации операции сложения
+двух объектов класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
+Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент
+первой строки первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+"""
+
+import copy
+
+
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        s = ''
+        for i in range(len(self.matrix)):
+            s = s + '\t'.join(map(str, self.matrix[i])) + '\n'
+        return s
+
+    def __add__(self, other):
+        if len(self.matrix) != len(other.matrix):
+            return None
+        res = copy.deepcopy(self.matrix)
+        for i in range(len(self.matrix)):
+            for k in range(len(self.matrix[i])):
+                res[i][k] = self.matrix[i][k] + other.matrix[i][k]
+        return Matrix(res)
+
+
+l1 = [[1, 2, 3], [4, 5, 6]]
+l2 = [[10, 20, 30], [40, 50, 60]]
+m1 = Matrix(l1)
+m2 = Matrix(l2)
+print(m1)
+print(m2)
+z = m1 + m2
+print(f' m1 + m2\n \n{z}')
